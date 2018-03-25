9a3468bb464c21b09e775a4e1cdd9181e93349b8
Musab <musab.on@gmail.com>
2018-03-25 09:41:10 +0300

    sweet

--- helloWolrd.js
+++ helloWolrd.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/sweety', (req, res) => res.send('Hetaf I love you'))
+app.get('/sweet', (req, res) => res.send('sweet'))
 
 
 app.listen(process.env.PORT || 3001, () => console.log('Example app listening on port 3001!'))
